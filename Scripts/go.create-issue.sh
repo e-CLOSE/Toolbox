0e964d9ae84aae94bce86ea1d161fd2eb288ed7d
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2022-02-12 20:21:48 +0100

    Slightly more advanced version

--- Scripts/go.create-issue.sh
+++ Scripts/go.create-issue.sh
@@ -5,10 +5,18 @@
 
 
 REPO_ROOT=""
+###########################################################################
 # Get last issue number
+# First define offset if there are some deleted issues, so that the
+# next issue to create will be ISSUE_OFFSET+LAST_ISSUE
+
+ISSUE_OFFSET=0 
 LAST_ISSUE=`gh issue list | sort -n | tail -1 | cut -f 1`
-let LAST_ISSUE=$LAST_ISSUE+1
+let LAST_ISSUE=$LAST_ISSUE+$ISSUE_OFFSET+1
 echo "Newly created issue will be [$LAST_ISSUE]"
+
+
+###########################################################################
 
 TITLE="Xournal++"
 DATE=`date -u`
@@ -25,7 +33,25 @@ Indexed in $README_URL"
 LABELS=("Whiteboard/notes" "*TOOL*")
 ASIGNEE="JaviMaciasG"
 
+INPUT_FILE="tools-mini.csv"
+DELIMITER="#"
+#while IFS=$'\t' read -r TITLE TYPE1 TYPE2 TYPE3 TYPE4 SUBSCRIPTION_MODE URL TESTED_BY COMMENTS PLATFORM
+while IFS='#' read -r TITLE TYPE1 TYPE2 TYPE3 TYPE4 SUBSCRIPTION_MODE URL TESTED_BY COMMENTS PLATFORM
+do
+    echo "---------------------------------------------------------------------------"
+    echo "TITLE=$TITLE"
+    echo "TYPE1=$TYPE1"
+    echo "TYPE2=$TYPE2"
+    echo "TYPE3=$TYPE3"
+    echo "TYPE4=$TYPE4"
+    echo "SUBSCRIPTION_MODE=$SUBSCRIPTION_MODE"
+    echo "URL=$URL"
+    echo "TESTED_BY=$TESTED_BY"
+    echo "COMMENTS=$COMMENTS"
+    echo "PLATFORM=$PLATFORM"
+done < <(cut -d "#" -f1,3,4,5,6,7,8,9,10,11 $INPUT_FILE | tail -n +2)
 
+      exit 1
 ###########################################################################
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
